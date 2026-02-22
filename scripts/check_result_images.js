4651b5a1641d2a090302a9565b67b9202f7f71b4
korean2012 <korean2012@gmail.com>
2026-02-22 13:11:20 +0000

    Unify page viewport frame and fix result asset mappings

--- scripts/check_result_images.js
+++ scripts/check_result_images.js
@@ -4,18 +4,9 @@ const path = require('path');
 const root = path.resolve(__dirname, '..');
 const imagesDir = path.join(root, 'assets', 'images', 'result');
 const images = [
-  'photo_aurora_bay.jpg',
-  'photo_han_river.jpg',
-  'photo_pine_ridge.jpg',
-  'photo_night_market.jpg',
-  'photo_dawn_field.jpg',
-  'photo_cloud_pass.jpg',
-  'photo_harbor_light.jpg',
-  'photo_skyline.jpg',
-  'photo_stone_valley.jpg',
-  'photo_tide_bridge.jpg',
-  'photo_city_pulse.jpg',
-  'photo_summit_mist.jpg',
+  'character_jeju_coast.svg',
+  'character_city_twilight.svg',
+  'character_mountain_mist.svg',
 ];
 
 const missing = images.filter((file) => !fs.existsSync(path.join(imagesDir, file)));
